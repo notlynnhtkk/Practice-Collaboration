216710c193fd8c8d2dfce936b3f7336ed0067198
notlynnhtkk <154324235+notlynnhtkk@users.noreply.github.com>
2023-12-19 11:29:49 +0100

    Add rearrange.py

---
+++ rearrange.py
@@ -0,0 +1,18 @@
+def rearrange_name(full_name):
+    # Split the full name into Last Name and First Name
+    last_name, first_name = full_name.split(', ')
+
+    # Rearrange and format the names
+    new_name = f"{first_name} {last_name}"
+
+    return new_name
+
+# Get input from the user
+input_name = input("Enter a name in 'Last Name, First Name' format: ")
+
+# Call the function to rearrange the name
+output_name = rearrange_name(input_name)
+
+# Display the result
+print(f"Input Name: {input_name}")
+print(f"Output Name: {output_name}")
